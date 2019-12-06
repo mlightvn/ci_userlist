0ff8b8247a236ca4013f576923843bc050fd50e8
Nguyen Nam <thietlalung@yahoo.com.vn>
2019-12-06 14:09:09 +0900

    password helper

--- application/helpers/password_helper.php
+++ application/helpers/password_helper.php
@@ -28,6 +28,10 @@ if(!function_exists('getHashedPassword'))
 if(!function_exists('generatePassword'))
 {
 	function generatePassword($length = 10, $has_special_char = false) {
+	    if ($length < 1) {
+	        throw new \RangeException("Length must be a positive integer");
+	    }
+
 		$options = ['number', 'letter'];
 		if($has_special_char){
 			$options[] = 'special_char';
@@ -45,8 +49,78 @@ if(!function_exists('generatePassword'))
 	    $charactersLength = strlen($characters);
 	    $randomString = '';
 	    for ($i = 0; $i < $length; $i++) {
-	        $randomString .= $characters[rand(0, $charactersLength - 1)];
+	        $randomString .= $characters[mt_rand(0, $charactersLength - 1)];
 	    }
 	    return $randomString;
 	}
 }
+
+if(!function_exists('random_string'))
+{
+	/**
+	 * Generate a random string, using a cryptographically secure 
+	 * pseudorandom number generator (random_int)
+	 *
+	 * This function uses type hints now (PHP 7+ only), but it was originally
+	 * written for PHP 5 as well.
+	 * 
+	 * For PHP 7, random_int is a PHP core function
+	 * For PHP 5.x, depends on https://github.com/paragonie/random_compat
+	 * 
+	 * @param int $length      How many characters do we want?
+	 * @param string $keyspace A string of all possible characters
+	 *                         to select from
+	 * @return string
+	 */
+	function random_string(
+	    int $length = 64,
+	    string $keyspace = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'
+	): string {
+	    if ($length < 1) {
+	        throw new \RangeException("Length must be a positive integer");
+	    }
+	    $pieces = [];
+	    $max = mb_strlen($keyspace, '8bit') - 1;
+	    for ($i = 0; $i < $length; ++$i) {
+	        $pieces []= $keyspace[random_int(0, $max)];
+	    }
+	    return implode('', $pieces);
+	}
+}
+
+if(!function_exists('shuffle_string'))
+{
+	function shuffle_string(
+	    int $length = 64,
+	    string $keyspace = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'
+	): string {
+	    if ($length < 1) {
+	        throw new \RangeException("Length must be a positive integer");
+	    }
+
+		$password = substr(str_shuffle($keyspace), 0, $length);
+	    return $password;
+	}
+}
+
+if(!function_exists('random_bytes_string'))
+{
+	function random_bytes_string(
+	    int $length = 64,
+	    string $keyspace = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'
+	): string {
+	    if ($length < 1) {
+	        throw new \RangeException("Length must be a positive integer");
+	    }
+
+		$bytes = random_bytes($length);
+		$password = bin2hex($bytes);
+	    return $password;
+	}
+}
+
+
+
+
+
+
